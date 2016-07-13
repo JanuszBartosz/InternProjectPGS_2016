1807ed5ab87ccf75219bbf983343ad0b8dbafd79
Adrian Kulinski (PGS Software) <akulinski@pgs-soft.com>
2016-07-13 10:45:37 +0200

    Checking if user is logged

--- LoyaltyProgram_Intern/src/main/java/com/pgs/soft/UserProfileValidator.java
+++ LoyaltyProgram_Intern/src/main/java/com/pgs/soft/UserProfileValidator.java
@@ -3,6 +3,9 @@ package com.pgs.soft;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -12,6 +15,9 @@ import com.pgs.soft.dto.UserProfileDTO;
 @Component
 public class UserProfileValidator implements Validator {
 
+	@Autowired
+	HttpSession session;
+	
 	@Override
 	public boolean supports(Class<?> clazz) {
 		return clazz.equals(UserProfileDTO.class);
@@ -20,6 +26,10 @@ public class UserProfileValidator implements Validator {
 	@Override
 	public void validate(Object target, Errors errors) {
 		UserProfileDTO userProfileDTO = (UserProfileDTO) target;
+		String userEmail = (String) session.getAttribute("email");
+		if(userEmail==null){
+			errors.reject("not.logged", "You are not logged.");
+		}
 		validateName(errors, userProfileDTO);
 		validateSurname(errors, userProfileDTO);
 	}
