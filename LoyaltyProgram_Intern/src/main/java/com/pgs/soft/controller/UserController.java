e3593e76118e47b370e949e93040798e163c9092
Adrian Kulinski (PGS Software) <akulinski@pgs-soft.com>
2016-07-18 13:18:38 +0200

    View and Controller

--- LoyaltyProgram_Intern/src/main/java/com/pgs/soft/controller/UserController.java
+++ LoyaltyProgram_Intern/src/main/java/com/pgs/soft/controller/UserController.java
@@ -6,26 +6,24 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.pgs.soft.ChangePasswordRequestValidator;
 import com.pgs.soft.RegisterValidator;
 import com.pgs.soft.UserProfileValidator;
+import com.pgs.soft.dto.ChangePasswordRequestDTO;
 import com.pgs.soft.dto.UserDTO;
 import com.pgs.soft.dto.UserProfileDTO;
 import com.pgs.soft.service.UserProfileService;
-
-import com.pgs.soft.ChangePasswordRequestValidator;
-import com.pgs.soft.dto.ChangePasswordRequestDTO;
-
 import com.pgs.soft.service.UserService;
 
 @Controller
-@ResponseBody
 public class UserController {
 	
 	@Autowired
@@ -64,10 +62,18 @@ public class UserController {
 		return "Profile filled.";
 	}
 	
+	@RequestMapping(value = "/register", method=RequestMethod.GET)
+	public String registerView(@ModelAttribute("userDTO") UserDTO userDTO){
+		return "register";
+	}
+	
 	@RequestMapping(value = "/register", method=RequestMethod.POST)
-	public String register(@Valid @RequestBody UserDTO userDTO){
+	public String register(@Valid @ModelAttribute("userDTO") UserDTO userDTO, BindingResult result){
+		if(result.hasErrors()){
+			return "register";
+		}
 		userService.save(userDTO);
-		return "Registered.";
+		return "register";
 	}
 
 	@RequestMapping(value = "/change_password", method=RequestMethod.POST)
