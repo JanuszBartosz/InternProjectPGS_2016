51c79e2b7aaebdcdb41636250fa6710ffdbe3f7d
Adrian Kulinski (PGS Software) <akulinski@pgs-soft.com>
2016-07-20 11:09:46 +0200

    [PLP-12] View added

--- LoyaltyProgram_Intern/src/main/java/com/pgs/soft/controller/CardController.java
+++ LoyaltyProgram_Intern/src/main/java/com/pgs/soft/controller/CardController.java
@@ -2,24 +2,27 @@ package com.pgs.soft.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pgs.soft.dto.CardDTO;
 import com.pgs.soft.service.CardService;
 
 @Controller
-@ResponseBody
 public class CardController {
-	
+
 	@Autowired
 	private CardService cardService;
 
+	@RequestMapping(value = "/card", method = RequestMethod.GET)
+	public String cardView(@ModelAttribute("cardDTO") CardDTO cardDTO) {
+		return "card";
+	}
+	
 	@RequestMapping(value = "/card", method = RequestMethod.POST)
-	public String card(@RequestBody CardDTO cardDTO){
+	public String card(@ModelAttribute("cardDTO") CardDTO cardDTO) {
 		cardService.save(cardDTO);
-		return "done";
+		return "card";
 	}
 }
