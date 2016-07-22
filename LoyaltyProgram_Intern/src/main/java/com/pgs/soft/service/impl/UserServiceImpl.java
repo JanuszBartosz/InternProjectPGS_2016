0a7133026826b05c4fd1ac54718a8446f0fa4a36
Adrian Kulinski (PGS Software) <akulinski@pgs-soft.com>
2016-07-22 14:01:45 +0200

    [PLP-20] ChangePassword - getting user from DB instead of conxtext

--- LoyaltyProgram_Intern/src/main/java/com/pgs/soft/service/impl/UserServiceImpl.java
+++ LoyaltyProgram_Intern/src/main/java/com/pgs/soft/service/impl/UserServiceImpl.java
@@ -74,8 +74,9 @@ public class UserServiceImpl extends GenericServiceImpl<User, UserDTO, Integer>
 	@Override
 	public void changePassword(ChangePasswordRequestDTO passwordDTO) {
 		User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		loggedUser.setPassword(bCryptPasswordEncoder.encode(passwordDTO.getNewPassword()));
-		userRepository.save(loggedUser);
+		User user = getUserByEmail(loggedUser.getEmail()).get();
+		user.setPassword(bCryptPasswordEncoder.encode(passwordDTO.getNewPassword()));
+		userRepository.save(user);
 	}
 
 	@Override
