c87ad30d6bc6d2e64a615315f93ec6201192c4e5
Bartosz Janusz (PGS Software) <bjanusz@pgs-soft.com>
2016-07-25 16:10:46 +0200

    [PLP-23] Validator change password test.

--- LoyaltyProgram_Intern/src/test/java/com/pgs/soft/service/impl/UserServiceImplTest.java
+++ LoyaltyProgram_Intern/src/test/java/com/pgs/soft/service/impl/UserServiceImplTest.java
@@ -3,18 +3,30 @@ package com.pgs.soft.service.impl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.pgs.soft.domain.Role;
 import com.pgs.soft.domain.User;
+import com.pgs.soft.dto.ChangePasswordRequestDTO;
 import com.pgs.soft.dto.UserDTO;
+import com.pgs.soft.repository.UserRepository;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceImplTest {
+
+	@Mock
+	UserRepository userRepository;
 
 	@InjectMocks
 	UserServiceImpl userServiceImpl = new UserServiceImpl();
@@ -66,29 +78,33 @@ public class UserServiceImplTest {
 		assertEquals(token, user.getRegistrationToken());
 	}
 
-	/*
-	 * @Test public void changePasswordTest() { // Given
-	 * ChangePasswordRequestDTO passwordDTO = new ChangePasswordRequestDTO();
-	 * String newPassword = "newpassword";
-	 * passwordDTO.setNewPassword(newPassword);
-	 * userServiceImpl.setbCryptPasswordEncoder(bCryptPasswordEncoder);
-	 * 
-	 * Integer id = new Integer(4); String email = "test@test.com"; String
-	 * password = "testpassword";
-	 * 
-	 * User loggedUser = new User(); loggedUser.setId(id);
-	 * loggedUser.setEmail(email); loggedUser.setPassword(password);
-	 * SecurityContext securityContext = Mockito.mock(SecurityContext.class);
-	 * Mockito.when(securityContext.getAuthentication().getPrincipal()).
-	 * thenReturn(loggedUser);
-	 * SecurityContextHolder.setContext(securityContext);
-	 * Mockito.when(userServiceImpl.getUserByEmail(loggedUser.getEmail()).get())
-	 * .thenReturn(loggedUser);
-	 * 
-	 * // When userServiceImpl.changePassword(passwordDTO);
-	 * 
-	 * // Then
-	 * 
-	 * }
-	 */
+	@Test
+	public void changePasswordTest() {
+		// Given
+		ChangePasswordRequestDTO passwordDTO = new ChangePasswordRequestDTO();
+		String newPassword = "newpassword";
+		passwordDTO.setNewPassword(newPassword);
+		userServiceImpl.setbCryptPasswordEncoder(bCryptPasswordEncoder);
+
+		Integer id = new Integer(4);
+		String email = "test@test.com";
+		String password = "testpassword";
+
+		User loggedUser = new User();
+		loggedUser.setId(id);
+		loggedUser.setEmail(email);
+		loggedUser.setPassword(password);
+
+		Authentication auth = new UsernamePasswordAuthenticationToken(loggedUser, null);
+		SecurityContextHolder.getContext().setAuthentication(auth);
+		Optional<User> user = null;
+		Mockito.when(userRepository.findOneByEmail(email)).thenReturn(user);
+
+		// When
+		userServiceImpl.changePassword(passwordDTO);
+
+		// Then
+		assertTrue(bCryptPasswordEncoder.matches(newPassword, loggedUser.getPassword()));
+	}
+
 }
