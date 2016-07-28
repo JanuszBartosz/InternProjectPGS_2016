e0cd63f4e60e6da3652d9f5cb1ade63febe0f2fd
Bartosz Janusz (PGS Software) <bjanusz@pgs-soft.com>
2016-07-28 12:13:54 +0200

    [PLP-31] ContactController.

---
+++ LoyaltyProgram_Intern/src/main/java/com/pgs/soft/controller/ContactController.java
@@ -0,0 +1,37 @@
+package com.pgs.soft.controller;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.pgs.soft.domain.User;
+import com.pgs.soft.dto.ContactMessageDTO;
+
+@Controller
+public class ContactController {
+
+	@RequestMapping(value = "/contact", method = RequestMethod.GET)
+	public ModelAndView doGet(@ModelAttribute ContactMessageDTO contactMessageDTO) {
+
+		if (SecurityContextHolder.getContext().getAuthentication() != null
+				&& SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+				&& !(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken)) {
+			User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			contactMessageDTO.setEmail(loggedUser.getEmail());
+		}
+
+		return new ModelAndView("contact_form");
+
+	}
+
+	@RequestMapping(value = "/contact", method = RequestMethod.POST)
+	public ModelAndView doPost(@ModelAttribute ContactMessageDTO contactMessageDTO) {
+
+		return new ModelAndView("index", "message", "Email sent successfully.");
+	}
+
+}
