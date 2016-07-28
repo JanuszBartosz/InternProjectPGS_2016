f3287e7566139d40a3d68330a1b43f3d5b16f25c
Adrian Kulinski (PGS Software) <akulinski@pgs-soft.com>
2016-07-28 13:32:59 +0200

    [PLP-28] Communication with microservice

--- LoyaltyProgram_Intern/src/main/java/com/pgs/soft/controller/AwardsController.java
+++ LoyaltyProgram_Intern/src/main/java/com/pgs/soft/controller/AwardsController.java
@@ -1,13 +1,14 @@
 package com.pgs.soft.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,24 +19,22 @@ import com.pgs.soft.dto.Category;
 public class AwardsController {
 
 	@RequestMapping(value = "/available_awards", method = RequestMethod.GET)
-	public ModelAndView awardsView() {
+	public ModelAndView awardsView(
+			@RequestParam(value = "sortBy", defaultValue = "name", required = false) String sortProperty,
+			@RequestParam(value = "direction", defaultValue = "ASC", required = false) Sort.Direction direction,
+			@RequestParam(value = "category", required = false) Category category) {
+
 		ModelAndView model = new ModelAndView("available_awards");
-
 		RestTemplate restTemplate = new RestTemplate();
-		Map<Category, List<AwardDTO>> map = new HashMap<>();// =
-															// restTemplate.getForObject("http://localhost:8080/awards",
-															// HashMap.class);
+		Map<Category, List<AwardDTO>> map = new HashMap<>();
 
-		List<AwardDTO> list1 = new ArrayList<>();
-		list1.add(new AwardDTO("Name1", "Description1", 1000, Category.Books));
-		list1.add(new AwardDTO("Name2", "Description2", 2000, Category.Books));
-
-		List<AwardDTO> list2 = new ArrayList<>();
-		list2.add(new AwardDTO("Name3", "Description3", 3000, Category.Toys));
-		list2.add(new AwardDTO("Name4", "Description4", 4000, Category.Toys));
-
-		map.put(Category.Books, list1);
-		map.put(Category.Toys, list2);
+		if (category != null) {
+			map = restTemplate.getForObject("http://localhost:9000/awards?category=" + category + "&sortBy="
+					+ sortProperty + "&direction=" + direction, Map.class);
+		}
+		else {
+			map = restTemplate.getForObject("http://localhost:9000/awards", Map.class);
+		}
 
 		model.addObject("map", map);
 		return model;
